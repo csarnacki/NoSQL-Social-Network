964676ecae4e8d96ecb23e5bd0085a76bb1a2f06
Chris <sirchrissarnacki@gmail.com>
2023-05-24 20:40:14 -0700

    Created server file to connect to server using Mongoose and Express packages

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const db = require('./config/connect');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+db.once('open', () => {
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+});
