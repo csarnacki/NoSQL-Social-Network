dc547736f7de9756eebe377f5f96eecfba980062
Chris <sirchrissarnacki@gmail.com>
2023-05-26 20:19:01 -0700

    Created Thoughts model and User model and index.js to export out classes

---
+++ models/Thoughts.js
@@ -0,0 +1,54 @@
+const { Schema, model, Types } = require('mongoose');
+
+const ResponseSchema = new Schema ({
+    id: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId(),
+    },
+
+    responseBody: {
+        type: String,
+        required: true,
+        maxlength: 200,
+    },
+
+    username: {
+        type: String,
+        required: true,
+    },
+
+    toJSON: {
+        getters: true,
+    },
+    id: false,
+});
+
+const ThoughtsSchema = new Schema ({
+    thoughtsText: {
+        type: String,
+        required: 'A thought needs to be provided',
+        minlength: 1,
+        maxlength: 200,
+    },
+
+    username: {
+        type: String,
+        required: true,
+    },
+
+    response: [ResponseSchema],
+    
+    toJSON: {
+        virtuals: true,
+        getters: true,
+    },
+    id: false,
+});
+
+ThoughtsSchema.virtual('reactionCount').get(function () {
+    return this.response.length;
+});
+
+const Thoughts = model('Thoughts', ThoughtsSchema);
+
+module.exports = Thoughts;
---
+++ models/User.js
@@ -0,0 +1,42 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema ({
+    username: {
+        type: String,
+        unique: true,
+        trim: true,
+        required: 'A username must be provided',
+    },
+
+    email: {
+        type: String,
+        unique: true,
+        required: 'A username must be provided',
+        match: [/.+@.+\..+/],
+
+        thoughts: [{
+            type: Schema.Types.ObjectId,
+            ref: 'Thoughts',
+        },
+    ],
+
+    friends: [{
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        },
+    ],
+    },
+
+    toJSON: {
+        virtuals: true,
+    },
+    id: false,
+});
+
+UserSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
